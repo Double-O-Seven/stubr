31bd60e8950a0938a0bdbf95322cdbff903920ad
leadrian <thewishwithin@gmail.com>
2020-01-13 22:54:45 +0100

    Clean up ConstructorStubber

--- stubr-core/src/main/java/ch/leadrian/stubr/core/stubber/ConstructorStubber.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/stubber/ConstructorStubber.java
@@ -8,6 +8,7 @@ import ch.leadrian.stubr.core.stubbingsite.StubbingSites;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
@@ -37,13 +38,8 @@ final class ConstructorStubber implements Stubber {
 
     @Override
     public Object stub(StubbingContext context, Type type) {
-        Constructor<?> constructor = getConstructor(type).orElseThrow(IllegalStateException::new);
-        Object[] parameterValues = stream(constructor.getParameters())
-                .map(parameter -> {
-                    ConstructorParameterStubbingSite site = StubbingSites.constructorParameter(context.getSite(), constructor, parameter);
-                    return context.getStubber().stub(parameter.getParameterizedType(), site);
-                })
-                .toArray(Object[]::new);
+        Constructor<?> constructor = getConstructor(type).orElseThrow(UnsupportedOperationException::new);
+        Object[] parameterValues = stub(context, constructor);
         try {
             return constructor.newInstance(parameterValues);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
@@ -51,22 +47,37 @@ final class ConstructorStubber implements Stubber {
         }
     }
 
+    private Object[] stub(StubbingContext context, Constructor<?> constructor) {
+        return stream(constructor.getParameters())
+                .map(parameter -> stub(context, constructor, parameter))
+                .toArray(Object[]::new);
+    }
+
+    private Object stub(StubbingContext context, Constructor<?> constructor, Parameter parameter) {
+        ConstructorParameterStubbingSite site = StubbingSites.constructorParameter(context.getSite(), constructor, parameter);
+        return context.getStubber().stub(parameter.getParameterizedType(), site);
+    }
+
     private Optional<Constructor<?>> getConstructor(Type type) {
         return getRawType(type).flatMap(this::getConstructor);
     }
 
-    private Optional<Constructor<?>> getConstructor(Class<?> targetClass) {
-        Constructor<?> constructor = constructorsByClass.computeIfAbsent(targetClass, clazz -> {
-            List<Constructor<?>> constructors = stream(clazz.getConstructors())
-                    .filter(c -> !c.isSynthetic() && !isPrivate(c.getModifiers()))
-                    .filter(constructorMatcher::matches)
-                    .collect(toList());
+    private Optional<Constructor<?>> getConstructor(Class<?> type) {
+        Constructor<?> constructor = constructorsByClass.computeIfAbsent(type, clazz -> {
+            List<Constructor<?>> constructors = getConstructors(clazz);
             if (constructors.size() == 1) {
                 return constructors.get(0);
             }
             return null;
         });
         return Optional.ofNullable(constructor);
+    }
+
+    private List<Constructor<?>> getConstructors(Class<?> type) {
+        return stream(type.getConstructors())
+                .filter(c -> !c.isSynthetic() && !isPrivate(c.getModifiers()))
+                .filter(constructorMatcher::matches)
+                .collect(toList());
     }
 
 }
