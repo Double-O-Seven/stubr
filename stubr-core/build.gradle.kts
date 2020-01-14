e8f9bc34e497fa5bd26eab7a161dca34d32fc72d
leadrian <thewishwithin@gmail.com>
2020-01-14 23:29:27 +0100

    Add jacoco test report

--- stubr-core/build.gradle.kts
+++ stubr-core/build.gradle.kts
@@ -1,3 +1,20 @@
+plugins {
+    jacoco
+}
+
+tasks {
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.isEnabled = true
+        }
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.4"
+}
+
 dependencies {
     implementation(group = "ch.leadrian.equalizer", name = "equalizer-core", version = "1.1.0")
     implementation(group = "com.google.guava", name = "guava", version = "28.2-jre")
