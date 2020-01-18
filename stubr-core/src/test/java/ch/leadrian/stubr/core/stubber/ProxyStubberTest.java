09b5e8c94fa3a78ac0450bfbcbce038c0ef30134
leadrian <thewishwithin@gmail.com>
2020-01-18 12:32:56 +0100

    Add test for non-caching ProxyStubber

--- stubr-core/src/test/java/ch/leadrian/stubr/core/stubber/ProxyStubberTest.java
+++ stubr-core/src/test/java/ch/leadrian/stubr/core/stubber/ProxyStubberTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.TestFactory;
 import java.util.stream.Stream;
 
 import static ch.leadrian.stubr.core.testing.StubberTester.stubberTester;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -14,19 +15,40 @@ class ProxyStubberTest {
     @TestFactory
     Stream<DynamicTest> testNonCachingProxyStubber() {
         return stubberTester()
-                .provideStub(int.class, 1337)
+                .provideStub(int.class, 1, 2, 3)
                 .provideStub("Test")
                 .accepts(Foo.class)
                 .andStubSatisfies(stub -> assertThat(stub).isInstanceOfSatisfying(Foo.class, foo -> {
                     assertAll(
-                            () -> assertThat(foo.getInt()).isEqualTo(1337),
+                            () -> assertThat(asList(foo.getInt(), foo.getInt(), foo.getInt())).containsExactly(1, 2, 3),
                             () -> assertThat(foo.getAnotherInt()).isEqualTo(1234),
                             () -> assertThat(foo.getString()).isEqualTo("Test")
                     );
                 }))
                 .rejects(Bar.class)
                 .rejects(Qux.class)
-                .test(Stubbers.proxy(false));
+                .test(
+                        Stubbers.proxy(false),
+                        Stubbers.proxy()
+                );
+    }
+
+    @TestFactory
+    Stream<DynamicTest> testCachingProxyStubber() {
+        return stubberTester()
+                .provideStub(int.class, 1, 2, 3)
+                .provideStub("Test")
+                .accepts(Foo.class)
+                .andStubSatisfies(stub -> assertThat(stub).isInstanceOfSatisfying(Foo.class, foo -> {
+                    assertAll(
+                            () -> assertThat(asList(foo.getInt(), foo.getInt(), foo.getInt())).containsExactly(1, 1, 1),
+                            () -> assertThat(foo.getAnotherInt()).isEqualTo(1234),
+                            () -> assertThat(foo.getString()).isEqualTo("Test")
+                    );
+                }))
+                .rejects(Bar.class)
+                .rejects(Qux.class)
+                .test(Stubbers.proxy(true));
     }
 
     private interface Foo {
