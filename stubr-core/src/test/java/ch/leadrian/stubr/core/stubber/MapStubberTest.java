ef5df8e234a4538ee087d290fdb9f89222a1c9e7
leadrian <thewishwithin@gmail.com>
2020-01-18 10:35:35 +0100

    Use StubberTester in MapStubberTest

--- stubr-core/src/test/java/ch/leadrian/stubr/core/stubber/MapStubberTest.java
+++ stubr-core/src/test/java/ch/leadrian/stubr/core/stubber/MapStubberTest.java
@@ -1,296 +1,91 @@
 package ch.leadrian.stubr.core.stubber;
 
-import ch.leadrian.stubr.core.RootStubber;
-import ch.leadrian.stubr.core.StubbingContext;
-import ch.leadrian.stubr.core.StubbingException;
 import ch.leadrian.stubr.core.stubbingsite.StubbingSites;
+import ch.leadrian.stubr.core.testing.ParameterizedTypeLiteral;
+import ch.leadrian.stubr.core.testing.TestStubbingSite;
 import ch.leadrian.stubr.core.type.TypeLiteral;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
-import static ch.leadrian.stubr.core.type.Types.getRawType;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.assertj.core.api.Assertions.entry;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static ch.leadrian.stubr.core.testing.StubberTester.stubberTester;
 
 class MapStubberTest {
 
-    private StubbingContext context;
-    private RootStubber rootStubber;
-
-    @BeforeEach
-    void setUp() {
-        rootStubber = mock(RootStubber.class);
-        when(rootStubber.stub(any(Type.class), any()))
-                .thenAnswer(invocation -> {
-                    Class<?> rawType = getRawType(invocation.getArgument(0, Type.class)).orElseThrow(AssertionError::new);
-                    if (rawType == String.class) {
-                        return "Test";
-                    } else if (rawType == Integer.class) {
-                        return 1234;
-                    } else if (rawType == Number.class) {
-                        return 1337;
-                    } else {
-                        throw new AssertionError();
-                    }
-                });
-        context = new StubbingContext(rootStubber, StubbingSites.unknown());
+    @TestFactory
+    Stream<DynamicTest> testEmptyMapStubber() {
+        return stubberTester()
+                .accepts(Map.class)
+                .andStubs(new HashMap<>())
+                .accepts(new TypeLiteral<Map<String, Integer>>() {
+                })
+                .andStubs(new HashMap<>())
+                .accepts(new TypeLiteral<Map<? super String, ? super Integer>>() {
+                })
+                .andStubs(new HashMap<>())
+                .accepts(new TypeLiteral<Map<?, ?>>() {
+                })
+                .andStubs(new HashMap<>())
+                .accepts(new TypeLiteral<Map<? extends String, ? extends Integer>>() {
+                })
+                .andStubs(new HashMap<>())
+                .rejects(HashMap.class)
+                .rejects(new TypeLiteral<HashMap<String, String>>() {
+                })
+                .rejects(new TypeLiteral<HashMap<? super String, ? super Integer>>() {
+                })
+                .rejects(new TypeLiteral<HashMap<? extends String, ? extends Integer>>() {
+                })
+                .test(
+                        Stubbers.map(Map.class, HashMap::new, context -> 0),
+                        Stubbers.map(Map.class, HashMap::new, 0),
+                        Stubbers.map(Map.class, HashMap::new)
+                );
     }
 
-    @Test
-    void givenMapSizeOfZeroItShouldAcceptNotParameterizedMap() {
-        Type type = Map.class;
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 0);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isTrue();
+    @TestFactory
+    Stream<DynamicTest> testNonEmptyMapStubber() {
+        ParameterizedTypeLiteral<Map<String, Integer>> mapOfStrings = new ParameterizedTypeLiteral<Map<String, Integer>>() {
+        };
+        return stubberTester()
+                .provideStub(String.class, "foo", "bar", "baz")
+                .provideStub(Integer.class, 123, 456, 789)
+                .rejects(Map.class)
+                .accepts(mapOfStrings)
+                .andStubs(new HashMap<>(ImmutableMap.of("foo", 123, "bar", 456, "baz", 789)))
+                .atSite(
+                        StubbingSites.parameterizedType(TestStubbingSite.INSTANCE, mapOfStrings.getType(), 0),
+                        StubbingSites.parameterizedType(TestStubbingSite.INSTANCE, mapOfStrings.getType(), 1),
+                        StubbingSites.parameterizedType(TestStubbingSite.INSTANCE, mapOfStrings.getType(), 0),
+                        StubbingSites.parameterizedType(TestStubbingSite.INSTANCE, mapOfStrings.getType(), 1),
+                        StubbingSites.parameterizedType(TestStubbingSite.INSTANCE, mapOfStrings.getType(), 0),
+                        StubbingSites.parameterizedType(TestStubbingSite.INSTANCE, mapOfStrings.getType(), 1)
+                )
+                .rejects(HashMap.class)
+                .rejects(new TypeLiteral<HashMap<String, Integer>>() {
+                })
+                .rejects(new TypeLiteral<HashMap<? super String, ? super Integer>>() {
+                })
+                .rejects(new TypeLiteral<HashMap<? extends String, ? extends Integer>>() {
+                })
+                .test(Stubbers.map(Map.class, HashMap::new, context -> 3), Stubbers.map(Map.class, HashMap::new, 3));
     }
 
-    @SuppressWarnings("unchecked")
-    @Test
-    void givenMapSizeOfZeroItShouldStubNotParameterizedMap() {
-        Type type = Map.class;
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 0);
-
-        Object stub = stubber.stub(context, type);
-
-        assertThat(stub)
-                .isInstanceOfSatisfying(HashMap.class, map -> assertThat(map).isEmpty());
+    @TestFactory
+    Stream<DynamicTest> testUnsupportedParameterization() {
+        return stubberTester()
+                .rejects(new TypeLiteral<WeirdMap<String, Integer, BigDecimal>>() {
+                })
+                .test(Stubbers.map(WeirdMap.class, values -> new WeirdMap(), context -> 3));
     }
 
-    @Test
-    void givenMapSizeGreaterThanZeroItShouldNotAcceptNotParameterizedMap() {
-        Type type = Map.class;
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isFalse();
-    }
-
-    @Test
-    void givenMapSizeOfZeroItShouldAcceptParameterizedMap() {
-        Type type = new TypeLiteral<Map<Integer, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 0);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isTrue();
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    void givenMapSizeOfZeroItShouldStubParameterizedMap() {
-        Type type = new TypeLiteral<Map<Integer, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 0);
-
-        Object stub = stubber.stub(context, type);
-
-        assertThat(stub)
-                .isInstanceOfSatisfying(HashMap.class, map -> assertThat(map).isEmpty());
-    }
-
-    @Test
-    void givenMapSizeGreaterThanZeroItShouldAcceptParameterizedMap() {
-        Type type = new TypeLiteral<Map<Integer, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isTrue();
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    void givenMapSizeGreaterThanZeroItShouldStubParameterizedMap() {
-        Type type = new TypeLiteral<Map<Integer, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        Object stub = stubber.stub(context, type);
-
-        assertThat(stub)
-                .isInstanceOfSatisfying(HashMap.class, map -> assertThat(map)
-                        .containsExactly(entry(1234, "Test")));
-    }
-
-    @Test
-    void shouldAcceptMapWithUpperBoundedWildcardType() {
-        Type type = new TypeLiteral<Map<? extends Number, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isTrue();
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    void shouldStubMapWithUpperBoundedWildcardType() {
-        Type type = new TypeLiteral<Map<? extends Number, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        Object stub = stubber.stub(context, type);
-
-        assertThat(stub)
-                .isInstanceOfSatisfying(HashMap.class, map -> assertThat(map)
-                        .containsExactly(entry(1337, "Test")));
-    }
-
-    @Test
-    void shouldAcceptMapWithLowerBoundedWildcardType() {
-        Type type = new TypeLiteral<Map<? super Number, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isTrue();
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    void shouldStubMapWithLowerBoundedWildcardType() {
-        Type type = new TypeLiteral<Map<? super Number, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        Object stub = stubber.stub(context, type);
-
-        assertThat(stub)
-                .isInstanceOfSatisfying(HashMap.class, map -> assertThat(map)
-                        .containsExactly(entry(1337, "Test")));
-    }
-
-    @Test
-    <T, U> void shouldAcceptMapWithTypeVariable() {
-        Type type = new TypeLiteral<Map<T, U>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isTrue();
-    }
-
-    @Test
-    <T> void shouldNotAcceptTypeVariable() {
-        Type type = new TypeLiteral<T>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isFalse();
-    }
-
-    @Test
-    <T> void shouldThrowExceptionWhenStubbingTypeVariable() {
-        Type type = new TypeLiteral<T>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        Throwable caughtThrowable = catchThrowable(() -> stubber.stub(context, type));
-
-        assertThat(caughtThrowable)
-                .isInstanceOf(StubbingException.class)
-                .hasMessage("Cannot stub T at UnknownStubbingSite");
-    }
-
-    @Test
-    <T> void shouldNotAcceptGenericArrayType() {
-        Type type = new TypeLiteral<T[]>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isFalse();
-    }
-
-    @Test
-    <T> void shouldThrowExceptionWhenStubbingGenericArrayType() {
-        Type type = new TypeLiteral<T[]>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        Throwable caughtThrowable = catchThrowable(() -> stubber.stub(context, type));
-
-        assertThat(caughtThrowable)
-                .isInstanceOf(StubbingException.class)
-                .hasMessage("Cannot stub T[] at UnknownStubbingSite");
-    }
-
-    @Test
-    void shouldNotAcceptMapThatIsNoExactMatch() {
-        Type type = HashMap.class;
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isFalse();
-    }
-
-    @Test
-    void shouldNotAcceptParameterizedMapThatIsNoExactMatch() {
-        Type type = new TypeLiteral<HashMap<Integer, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isFalse();
-    }
-
-    @Test
-    void shouldNotAcceptNonMapType() {
-        Type type = Number.class;
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        boolean accepts = stubber.accepts(context, type);
-
-        assertThat(accepts)
-                .isFalse();
-    }
-
-    @Test
-    void shouldUseParameterizedTypeStubbingSite() {
-        Type type = new TypeLiteral<Map<Integer, String>>() {
-        }.getType();
-        MapStubber stubber = new MapStubber<>(Map.class, HashMap::new, context -> 1);
-
-        stubber.stub(context, type);
-
-        verify(rootStubber, times(2)).stub(any(Type.class), any());
-        verify(rootStubber).stub((Type) Integer.class, StubbingSites.parameterizedType(StubbingSites.unknown(), (ParameterizedType) type, 0));
-        verify(rootStubber).stub((Type) String.class, StubbingSites.parameterizedType(StubbingSites.unknown(), (ParameterizedType) type, 1));
+    @SuppressWarnings("unused")
+    private static final class WeirdMap<T, U, V> extends HashMap<T, U> {
     }
 
 }
