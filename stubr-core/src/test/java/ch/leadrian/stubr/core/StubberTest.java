232b0940b31d31531b88e111046fff8b9c54e7e7
leadrian <thewishwithin@gmail.com>
2020-01-21 00:31:34 +0100

    Fix @Test import

--- stubr-core/src/test/java/ch/leadrian/stubr/core/StubberTest.java
+++ stubr-core/src/test/java/ch/leadrian/stubr/core/StubberTest.java
@@ -1,6 +1,6 @@
 package ch.leadrian.stubr.core;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
--- stubr-core/src/test/java/ch/leadrian/stubr/core/matcher/AnnotatedWithConstructorMatcherTest.java
+++ stubr-core/src/test/java/ch/leadrian/stubr/core/matcher/AnnotatedWithConstructorMatcherTest.java
@@ -1,7 +1,7 @@
 package ch.leadrian.stubr.core.matcher;
 
 import com.google.common.testing.EqualsTester;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
