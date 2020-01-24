faa126a87154a978064b8d307324fd18298baef6
leadrian <thewishwithin@gmail.com>
2020-01-24 18:57:19 +0100

    Add StubbingSite#walk

--- stubr-core/src/main/java/ch/leadrian/stubr/core/StubbingSite.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/StubbingSite.java
@@ -1,9 +1,16 @@
 package ch.leadrian.stubr.core;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface StubbingSite {
 
     Optional<? extends StubbingSite> getParent();
+
+    default Stream<StubbingSite> walk() {
+        return getParent()
+                .map(parent -> Stream.concat(Stream.of(this), parent.walk()))
+                .orElseGet(() -> Stream.of(this));
+    }
 
 }
---
+++ stubr-core/src/test/java/ch/leadrian/stubr/core/StubbingSiteTest.java
@@ -0,0 +1,52 @@
+package ch.leadrian.stubr.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StubbingSiteTest {
+
+    @Test
+    void givenParentItShouldReturnSitesUpToRoot() {
+        StubbingSite parentOfParent = new TestStubbingSite();
+        StubbingSite parent = new TestStubbingSite(parentOfParent);
+        StubbingSite stubbingSite = new TestStubbingSite(parent);
+
+        Stream<StubbingSite> stubbingSites = stubbingSite.walk();
+
+        assertThat(stubbingSites)
+                .containsExactly(stubbingSite, parent, parentOfParent);
+    }
+
+    @Test
+    void givenNoParentItShouldReturnStreamOfSelf() {
+        StubbingSite stubbingSite = new TestStubbingSite();
+
+        Stream<StubbingSite> stubbingSites = stubbingSite.walk();
+
+        assertThat(stubbingSites)
+                .containsExactly(stubbingSite);
+    }
+
+    private static final class TestStubbingSite implements StubbingSite {
+
+        private final StubbingSite parent;
+
+        private TestStubbingSite(StubbingSite parent) {
+            this.parent = parent;
+        }
+
+        private TestStubbingSite() {
+            this.parent = null;
+        }
+
+        @Override
+        public Optional<? extends StubbingSite> getParent() {
+            return Optional.ofNullable(parent);
+        }
+    }
+
+}
