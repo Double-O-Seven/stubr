fc49da84422e04f7fd57cc643f17c2bf0f349c47
leadrian <thewishwithin@gmail.com>
2020-01-24 19:28:23 +0100

    Remove unnecessary nesting

--- stubr-integration-test/src/test/java/ch/leadrian/stubr/integrationtest/CoreIntegrationTest.java
+++ stubr-integration-test/src/test/java/ch/leadrian/stubr/integrationtest/CoreIntegrationTest.java
@@ -24,231 +24,226 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class CoreIntegrationTest {
 
+    private TestData testData;
+
+    @BeforeEach
+    void setUp() {
+        RootStubber rootStubber = RootStubber.builder()
+                .include(defaultRootStubber())
+                .stubWith(defaultCollections(3))
+                .stubWith(suppliedValue(String.class, sequenceNumber -> String.format("value%d", sequenceNumber)))
+                .stubWith(suppliedValue(Integer.class, sequenceNumber -> sequenceNumber))
+                .stubWith(collection(List.class, ArrayList::new, this::getCollectionSize).when(annotatedSiteIs(annotatedWith(CollectionSize.class))))
+                .build();
+        testData = rootStubber.stub(TestData.class);
+    }
+
+    private int getCollectionSize(StubbingContext context) {
+        AnnotatedStubbingSite annotatedSite = (AnnotatedStubbingSite) context.getSite();
+        return annotatedSite.getAnnotatedElement().getAnnotation(CollectionSize.class).value();
+    }
+
+    @Test
+    void shouldStubTestData() {
+        assertAll(
+                () -> assertThat(testData.getPrimitives()).isNotNull(),
+                () -> assertThat(testData.getPrimitiveWrappers()).isNotNull(),
+                () -> assertThat(testData.getArrays()).isNotNull(),
+                () -> assertThat(testData.getCollections()).isNotNull()
+        );
+    }
+
     @Nested
-    class DefaultStubsTest {
+    class ArraysTest {
 
-        private TestData testData;
-
-        @BeforeEach
-        void setUp() {
-            RootStubber rootStubber = RootStubber.builder()
-                    .include(defaultRootStubber())
-                    .stubWith(defaultCollections(3))
-                    .stubWith(suppliedValue(String.class, sequenceNumber -> String.format("value%d", sequenceNumber)))
-                    .stubWith(suppliedValue(Integer.class, sequenceNumber -> sequenceNumber))
-                    .stubWith(collection(List.class, ArrayList::new, this::getCollectionSize).when(annotatedSiteIs(annotatedWith(CollectionSize.class))))
-                    .build();
-            testData = rootStubber.stub(TestData.class);
-        }
-
-        private int getCollectionSize(StubbingContext context) {
-            AnnotatedStubbingSite annotatedSite = (AnnotatedStubbingSite) context.getSite();
-            return annotatedSite.getAnnotatedElement().getAnnotation(CollectionSize.class).value();
+        @Test
+        void shouldStubBooleanArray() {
+            assertThat(testData.getArrays().getBooleanArray()).containsExactly(false, false, false);
         }
 
         @Test
-        void shouldStubTestData() {
-            assertAll(
-                    () -> assertThat(testData.getPrimitives()).isNotNull(),
-                    () -> assertThat(testData.getPrimitiveWrappers()).isNotNull(),
-                    () -> assertThat(testData.getArrays()).isNotNull(),
-                    () -> assertThat(testData.getCollections()).isNotNull()
-            );
+        void shouldStubByteArray() {
+            assertThat(testData.getArrays().getByteArray()).containsExactly(0, 0, 0);
         }
 
-        @Nested
-        class ArraysTest {
-
-            @Test
-            void shouldStubBooleanArray() {
-                assertThat(testData.getArrays().getBooleanArray()).containsExactly(false, false, false);
-            }
-
-            @Test
-            void shouldStubByteArray() {
-                assertThat(testData.getArrays().getByteArray()).containsExactly(0, 0, 0);
-            }
-
-            @Test
-            void shouldStubShortArray() {
-                assertThat(testData.getArrays().getShortArray()).containsExactly((short) 0, (short) 0, (short) 0);
-            }
-
-            @Test
-            void shouldStubCharArray() {
-                assertThat(testData.getArrays().getCharArray()).containsExactly('\0', '\0', '\0');
-            }
-
-            @Test
-            void shouldStubIntArray() {
-                assertThat(testData.getArrays().getIntArray()).containsExactly(0, 0, 0);
-            }
-
-            @Test
-            void shouldStubLongArray() {
-                assertThat(testData.getArrays().getLongArray()).containsExactly(0L, 0L, 0L);
-            }
-
-            @Test
-            void shouldStubFloatArray() {
-                assertThat(testData.getArrays().getFloatArray()).containsExactly(0f, 0f, 0f);
-            }
-
-            @Test
-            void shouldStubDoubleArray() {
-                assertThat(testData.getArrays().getDoubleArray()).containsExactly(0.0, 0.0, 0.0);
-            }
-
-            @Test
-            void shouldStubStringArray() {
-                assertThat(testData.getArrays().getStringArray()).containsExactly("value0", "value1", "value2");
-            }
-
+        @Test
+        void shouldStubShortArray() {
+            assertThat(testData.getArrays().getShortArray()).containsExactly((short) 0, (short) 0, (short) 0);
         }
 
-        @Nested
-        class AnnotationsTest {
-
-            @Test
-            void shouldReturnEmptyList() {
-                assertThat(testData.getAnnotations().getEmptyList()).isEmpty();
-            }
-
-            @Test
-            void shouldReturnListOfFiveStrings() {
-                assertThat(testData.getAnnotations().getListOfFiveStrings()).containsExactly("value0", "value1", "value2", "value3", "value4");
-            }
-
-            @Test
-            void shouldReturnListOfTwoStrings() {
-                assertThat(testData.getAnnotations().getListOfTwoStrings()).containsExactly("value0", "value1");
-            }
-
+        @Test
+        void shouldStubCharArray() {
+            assertThat(testData.getArrays().getCharArray()).containsExactly('\0', '\0', '\0');
         }
 
-        @Nested
-        class CollectionsTest {
-
-            @Test
-            void shouldStubCollection() {
-                assertThat(testData.getCollections().getCollection()).containsExactly("value0", "value1", "value2");
-            }
-
-            @Test
-            void shouldStubList() {
-                assertThat(testData.getCollections().getList()).containsExactly("value0", "value1", "value2");
-            }
-
-            @Test
-            void shouldStubSet() {
-                assertThat(testData.getCollections().getSet()).containsExactlyInAnyOrder("value0", "value1", "value2");
-            }
-
-            @Test
-            void shouldStubNavigableSet() {
-                assertThat(testData.getCollections().getNavigableSet()).containsExactly("value0", "value1", "value2");
-            }
-
-            @Test
-            void shouldStubMap() {
-                assertThat(testData.getCollections().getMap()).containsOnly(entry(0, "value0"), entry(1, "value1"), entry(2, "value2"));
-            }
-
-            @Test
-            void shouldStubNavigableMap() {
-                assertThat(testData.getCollections().getNavigableMap()).containsExactly(entry(0, "value0"), entry(1, "value1"), entry(2, "value2"));
-            }
-
+        @Test
+        void shouldStubIntArray() {
+            assertThat(testData.getArrays().getIntArray()).containsExactly(0, 0, 0);
         }
 
-        @Nested
-        class PrimitivesTest {
-
-            @Test
-            void shouldStubBoolean() {
-                assertThat(testData.getPrimitives().getBoolean()).isFalse();
-            }
-
-            @Test
-            void shouldStubByte() {
-                assertThat(testData.getPrimitives().getByte()).isZero();
-            }
-
-            @Test
-            void shouldStubShort() {
-                assertThat(testData.getPrimitives().getShort()).isZero();
-            }
-
-            @Test
-            void shouldStubChar() {
-                assertThat(testData.getPrimitives().getChar()).isEqualTo('\0');
-            }
-
-            @Test
-            void shouldStubInt() {
-                assertThat(testData.getPrimitives().getInt()).isZero();
-            }
-
-            @Test
-            void shouldStubLong() {
-                assertThat(testData.getPrimitives().getLong()).isZero();
-            }
-
-            @Test
-            void shouldStubFloat() {
-                assertThat(testData.getPrimitives().getFloat()).isZero();
-            }
-
-            @Test
-            void shouldStubDouble() {
-                assertThat(testData.getPrimitives().getDouble()).isZero();
-            }
-
+        @Test
+        void shouldStubLongArray() {
+            assertThat(testData.getArrays().getLongArray()).containsExactly(0L, 0L, 0L);
         }
 
-        @Nested
-        class PrimitiveWrappersTest {
+        @Test
+        void shouldStubFloatArray() {
+            assertThat(testData.getArrays().getFloatArray()).containsExactly(0f, 0f, 0f);
+        }
 
-            @Test
-            void shouldStubBooleanWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getBoolean()).isFalse();
-            }
+        @Test
+        void shouldStubDoubleArray() {
+            assertThat(testData.getArrays().getDoubleArray()).containsExactly(0.0, 0.0, 0.0);
+        }
 
-            @Test
-            void shouldStubByteWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getByte()).isZero();
-            }
+        @Test
+        void shouldStubStringArray() {
+            assertThat(testData.getArrays().getStringArray()).containsExactly("value0", "value1", "value2");
+        }
 
-            @Test
-            void shouldStubShortWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getShort()).isZero();
-            }
+    }
 
-            @Test
-            void shouldStubCharWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getCharacter()).isEqualTo('\0');
-            }
+    @Nested
+    class AnnotationsTest {
 
-            @Test
-            void shouldStubIntWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getInteger()).isZero();
-            }
+        @Test
+        void shouldReturnEmptyList() {
+            assertThat(testData.getAnnotations().getEmptyList()).isEmpty();
+        }
 
-            @Test
-            void shouldStubLongWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getLong()).isZero();
-            }
+        @Test
+        void shouldReturnListOfFiveStrings() {
+            assertThat(testData.getAnnotations().getListOfFiveStrings()).containsExactly("value0", "value1", "value2", "value3", "value4");
+        }
 
-            @Test
-            void shouldStubFloatWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getFloat()).isZero();
-            }
+        @Test
+        void shouldReturnListOfTwoStrings() {
+            assertThat(testData.getAnnotations().getListOfTwoStrings()).containsExactly("value0", "value1");
+        }
 
-            @Test
-            void shouldStubDoubleWrapper() {
-                assertThat(testData.getPrimitiveWrappers().getDouble()).isZero();
-            }
+    }
 
+    @Nested
+    class CollectionsTest {
+
+        @Test
+        void shouldStubCollection() {
+            assertThat(testData.getCollections().getCollection()).containsExactly("value0", "value1", "value2");
+        }
+
+        @Test
+        void shouldStubList() {
+            assertThat(testData.getCollections().getList()).containsExactly("value0", "value1", "value2");
+        }
+
+        @Test
+        void shouldStubSet() {
+            assertThat(testData.getCollections().getSet()).containsExactlyInAnyOrder("value0", "value1", "value2");
+        }
+
+        @Test
+        void shouldStubNavigableSet() {
+            assertThat(testData.getCollections().getNavigableSet()).containsExactly("value0", "value1", "value2");
+        }
+
+        @Test
+        void shouldStubMap() {
+            assertThat(testData.getCollections().getMap()).containsOnly(entry(0, "value0"), entry(1, "value1"), entry(2, "value2"));
+        }
+
+        @Test
+        void shouldStubNavigableMap() {
+            assertThat(testData.getCollections().getNavigableMap()).containsExactly(entry(0, "value0"), entry(1, "value1"), entry(2, "value2"));
+        }
+
+    }
+
+    @Nested
+    class PrimitivesTest {
+
+        @Test
+        void shouldStubBoolean() {
+            assertThat(testData.getPrimitives().getBoolean()).isFalse();
+        }
+
+        @Test
+        void shouldStubByte() {
+            assertThat(testData.getPrimitives().getByte()).isZero();
+        }
+
+        @Test
+        void shouldStubShort() {
+            assertThat(testData.getPrimitives().getShort()).isZero();
+        }
+
+        @Test
+        void shouldStubChar() {
+            assertThat(testData.getPrimitives().getChar()).isEqualTo('\0');
+        }
+
+        @Test
+        void shouldStubInt() {
+            assertThat(testData.getPrimitives().getInt()).isZero();
+        }
+
+        @Test
+        void shouldStubLong() {
+            assertThat(testData.getPrimitives().getLong()).isZero();
+        }
+
+        @Test
+        void shouldStubFloat() {
+            assertThat(testData.getPrimitives().getFloat()).isZero();
+        }
+
+        @Test
+        void shouldStubDouble() {
+            assertThat(testData.getPrimitives().getDouble()).isZero();
+        }
+
+    }
+
+    @Nested
+    class PrimitiveWrappersTest {
+
+        @Test
+        void shouldStubBooleanWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getBoolean()).isFalse();
+        }
+
+        @Test
+        void shouldStubByteWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getByte()).isZero();
+        }
+
+        @Test
+        void shouldStubShortWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getShort()).isZero();
+        }
+
+        @Test
+        void shouldStubCharWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getCharacter()).isEqualTo('\0');
+        }
+
+        @Test
+        void shouldStubIntWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getInteger()).isZero();
+        }
+
+        @Test
+        void shouldStubLongWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getLong()).isZero();
+        }
+
+        @Test
+        void shouldStubFloatWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getFloat()).isZero();
+        }
+
+        @Test
+        void shouldStubDoubleWrapper() {
+            assertThat(testData.getPrimitiveWrappers().getDouble()).isZero();
         }
 
     }
