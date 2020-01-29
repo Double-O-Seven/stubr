968b098a4cab2e92fd556834c272a7723573d2ff
leadrian <thewishwithin@gmail.com>
2020-01-29 21:39:14 +0100

    Add javadoc for RootStubbers

--- stubr-core/src/main/java/ch/leadrian/stubr/core/RootStubbers.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/RootStubbers.java
@@ -18,6 +18,9 @@ import static ch.leadrian.stubr.core.stubber.Stubbers.optional;
 import static ch.leadrian.stubr.core.stubber.Stubbers.proxy;
 import static ch.leadrian.stubr.core.stubber.Stubbers.rootStubber;
 
+/**
+ * Class containing factory methods for basic {@link RootStubber}s.
+ */
 public final class RootStubbers {
 
     private static final RootStubber DEFAULT_ROOT_STUBBER = RootStubber.builder()
@@ -44,10 +47,37 @@ public final class RootStubbers {
     private RootStubbers() {
     }
 
+    /**
+     * Provides a stateless {@link RootStubber} that will stub the following stub values:
+     * <ul>
+     * <li>Default values for primitives and their wrappers</li>
+     * <li>{@link java.lang.reflect.Proxy} instances for interfaces that return stub values for non-void method
+     * calls</li>
+     * <li>Enum values</li>
+     * <li>Non-empty {@link java.util.Optional}s</li>
+     * <li>Object that can be created using a default constructor</li>
+     * <li>Object that can be created using a non-default constructor</li>
+     * <li>Object that can be created using a factory method</li>
+     * <li>Common collections and array, filled with a single element</li>
+     * <li>Reasonable constant values for commonly used immutable classes such as {@link java.time.LocalDate}</li>
+     * <li>Reasonable constant values for commonly used mutable classes such as {@link java.util.Date}</li>
+     * </ul>
+     *
+     * @return a {@link RootStubber} stubbing non-null, non-empty default values.
+     * @see ch.leadrian.stubr.core.stubber.Stubbers
+     */
     public static RootStubber defaultRootStubber() {
         return DEFAULT_ROOT_STUBBER;
     }
 
+    /**
+     * Provides a stateless {@link RootStubber} that uses {@link RootStubbers#defaultRootStubber()} as a baseline.
+     * However, default collections, {@link java.util.Optional}s and arrays are kept empty and nullable sites will
+     * receive {@code null} as stub value.
+     *
+     * @return a {@link RootStubber} stubbing nullable, empty default values.
+     * @see ch.leadrian.stubr.core.stubber.Stubbers
+     */
     public static RootStubber minimalRootStubber() {
         return MINIMAL_ROOT_STUBBER;
     }
