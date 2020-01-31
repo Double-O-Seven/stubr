a8403105871dcff984bb0c04615692c633b41770
leadrian <thewishwithin@gmail.com>
2020-01-31 23:31:12 +0100

    Add javadoc for remaining stubbing site implementations

--- stubr-core/src/main/java/ch/leadrian/stubr/core/site/ParameterStubbingSite.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/site/ParameterStubbingSite.java
@@ -2,10 +2,21 @@ package ch.leadrian.stubr.core.site;
 
 import java.lang.reflect.Parameter;
 
+/**
+ * Stubbing site indicating that the current stubbing site is a {@link Parameter}.
+ */
 public interface ParameterStubbingSite extends AnnotatedStubbingSite {
 
+    /**
+     * Returns the parameter where the stubbing takes place.
+     *
+     * @return the parameter
+     */
     Parameter getParameter();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     default Parameter getAnnotatedElement() {
         return getParameter();
--- stubr-core/src/main/java/ch/leadrian/stubr/core/site/UnknownStubbingSite.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/site/UnknownStubbingSite.java
@@ -4,14 +4,25 @@ import ch.leadrian.stubr.core.StubbingSite;
 
 import java.util.Optional;
 
+/**
+ * A {@link StubbingSite} indicating that the current stubbing site is not known.
+ * <p>
+ * By default, a {@link ch.leadrian.stubr.core.Stubber} uses this site if none other is given.
+ */
 public enum UnknownStubbingSite implements StubbingSite {
     INSTANCE;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<? extends StubbingSite> getParent() {
         return Optional.empty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return getClass().getSimpleName();
