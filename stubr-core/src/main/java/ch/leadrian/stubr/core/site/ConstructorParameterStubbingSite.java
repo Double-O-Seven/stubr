0596ba33b1de702981bb3870bf8f3a6d362d1c85
leadrian <thewishwithin@gmail.com>
2020-01-31 22:21:08 +0100

    Add javadoc for Constructor stubbing sites

--- stubr-core/src/main/java/ch/leadrian/stubr/core/site/ConstructorParameterStubbingSite.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/site/ConstructorParameterStubbingSite.java
@@ -11,6 +11,9 @@ import static ch.leadrian.equalizer.Equalizer.equalsAndHashCodeBuilder;
 import static com.google.common.base.MoreObjects.toStringHelper;
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Stubbing site indicating that the current stubbing site is a constructor parameter.
+ */
 public final class ConstructorParameterStubbingSite implements ConstructorStubbingSite, ParameterStubbingSite {
 
     private static final EqualsAndHashCode<ConstructorParameterStubbingSite> EQUALS_AND_HASH_CODE = equalsAndHashCodeBuilder(ConstructorParameterStubbingSite.class)
@@ -32,31 +35,49 @@ public final class ConstructorParameterStubbingSite implements ConstructorStubbi
         this.parameter = parameter;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<StubbingSite> getParent() {
         return Optional.of(parent);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Constructor<?> getConstructor() {
         return constructor;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Parameter getParameter() {
         return parameter;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object obj) {
         return EQUALS_AND_HASH_CODE.equals(this, obj);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return EQUALS_AND_HASH_CODE.hashCode(this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return toStringHelper(this)
--- stubr-core/src/main/java/ch/leadrian/stubr/core/site/ConstructorStubbingSite.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/site/ConstructorStubbingSite.java
@@ -4,8 +4,16 @@ import ch.leadrian.stubr.core.StubbingSite;
 
 import java.lang.reflect.Constructor;
 
+/**
+ * Stubbing site indicating that the current stubbing site is a constructor.
+ */
 public interface ConstructorStubbingSite extends StubbingSite {
 
+    /**
+     * Returns the constructor where the stubbing takes place.
+     *
+     * @return the constructor
+     */
     Constructor<?> getConstructor();
 
 }
