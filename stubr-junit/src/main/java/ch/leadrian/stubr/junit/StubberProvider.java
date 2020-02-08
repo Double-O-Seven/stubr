0bae3890dd085000bdaa0edd5b4ad0dd5e13afe6
leadrian <thewishwithin@gmail.com>
2020-02-08 11:37:22 +0100

    Add javadoc to providers

--- stubr-junit/src/main/java/ch/leadrian/stubr/junit/StubberProvider.java
+++ stubr-junit/src/main/java/ch/leadrian/stubr/junit/StubberProvider.java
@@ -5,8 +5,26 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.List;
 
+/**
+ * A class used by {@link ch.leadrian.stubr.junit.annotation.Include} to determine which {@link Stubber}s should be
+ * included when configuring a {@link Stubber} for a test case.
+ * <p>
+ * Any implementation must provide a default constructor since all instances are instantiated through reflection.
+ *
+ * @see ch.leadrian.stubr.junit.annotation.Include
+ */
 public interface StubberProvider {
 
+    /**
+     * Returns a list of {@link Stubber}s that should be included when configuring a {@link Stubber} for a test case.
+     * <p>
+     * The {@link Stubber}s will be included in the {@link Stubber} in the order in which they are present in the given
+     * list.
+     *
+     * @param extensionContext the current extension context
+     * @return a list of {@link Stubber}s that should be included when configuring a {@link Stubber} for a test case
+     * @see ch.leadrian.stubr.core.StubberBuilder#include(Stubber)
+     */
     List<? extends Stubber> getStubbers(ExtensionContext extensionContext);
 
 }
--- stubr-junit/src/main/java/ch/leadrian/stubr/junit/StubbingStrategyProvider.java
+++ stubr-junit/src/main/java/ch/leadrian/stubr/junit/StubbingStrategyProvider.java
@@ -1,12 +1,33 @@
 package ch.leadrian.stubr.junit;
 
+import ch.leadrian.stubr.core.Stubber;
 import ch.leadrian.stubr.core.StubbingStrategy;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.List;
 
+/**
+ * A class used by {@link ch.leadrian.stubr.junit.annotation.StubWith} to determine which {@link StubbingStrategy}s
+ * should be used when configuring a {@link Stubber} for a test case.
+ * <p>
+ * Any implementation must provide a default constructor since all instances are instantiated through reflection.
+ *
+ * @see ch.leadrian.stubr.junit.annotation.Include
+ */
 public interface StubbingStrategyProvider {
 
+    /**
+     * Returns a list of {@link StubbingStrategy}s that should be included when configuring a {@link Stubber} for a test
+     * case.
+     * <p>
+     * The {@link StubbingStrategy}s will be added to the {@link Stubber} in the order in which they are present in the
+     * given list.
+     *
+     * @param extensionContext the current extension context
+     * @return a list of {@link StubbingStrategy}s that should be used when configuring a {@link Stubber} for a test
+     * case
+     * @see ch.leadrian.stubr.core.StubberBuilder#stubWith(StubbingStrategy)
+     */
     List<? extends StubbingStrategy> getStubbingStrategies(ExtensionContext extensionContext);
 
 }
