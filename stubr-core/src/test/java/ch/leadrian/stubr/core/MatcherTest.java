73286c4246e549e672a2c6f85737a1a29d894b6d
leadrian <thewishwithin@gmail.com>
2020-04-02 00:36:10 +0200

    Remove unnecessary type param

--- stubr-core/src/test/java/ch/leadrian/stubr/core/MatcherTest.java
+++ stubr-core/src/test/java/ch/leadrian/stubr/core/MatcherTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
-class MatcherTest<T> {
+class MatcherTest {
 
     @ParameterizedTest
     @CsvSource({
