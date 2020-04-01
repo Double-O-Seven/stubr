7a66750d235f1c5d35d71692de793d2b0c8a9a53
leadrian <thewishwithin@gmail.com>
2020-04-02 00:35:27 +0200

    Add selector package-info

---
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/selector/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing default implementations of {@link ch.leadrian.stubr.core.Selector}.
+ * <p>
+ * Concrete instances can be created using factory methods from {@link ch.leadrian.stubr.core.selector.Selectors}.
+ */
+package ch.leadrian.stubr.core.selector;
