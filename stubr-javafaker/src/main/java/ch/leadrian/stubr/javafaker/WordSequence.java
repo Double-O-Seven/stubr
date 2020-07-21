ff1043cd8e0bf0c35486b3a6407e2d139a8e10a8
leadrian <thewishwithin@gmail.com>
2020-07-21 22:16:02 +0200

    Add javadoc for WordSequence

--- stubr-javafaker/src/main/java/ch/leadrian/stubr/javafaker/WordSequence.java
+++ stubr-javafaker/src/main/java/ch/leadrian/stubr/javafaker/WordSequence.java
@@ -29,10 +29,25 @@ import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
+/**
+ * This class represents a sequence of words that is extracted from a lower camel-case identifier, such as a method name
+ * or parameter name for example. All words are stored in lower case.
+ * <p>
+ * Calling {@link WordSequence#extractFrom(String)} with a string value "getFirstName" for example results in a sequence
+ * containing "get", "first" and "name".
+ */
 public final class WordSequence {
 
     private final List<String> words;
 
+    /**
+     * Returns the word sequence extracted from the given {@code value}.
+     * <p>
+     * Passing a string value "getFirstName" for example results in a sequence containing "get", "first" and "name".
+     *
+     * @param value the passed string representing of a camel-case identifier.
+     * @return the word sequence extracted from the given {@code value}
+     */
     public static WordSequence extractFrom(String value) {
         List<String> words = ImmutableList.copyOf(LOWER_CAMEL.to(LOWER_HYPHEN, value).split("-"));
         return new WordSequence(words);
@@ -42,15 +57,67 @@ public final class WordSequence {
         this.words = words;
     }
 
+    /**
+     * Returns {@code true} if the word sequence contains the given word, else {@code false}.
+     * <p>
+     * The check is performed case-insensitively.
+     *
+     * @param word the word possibly contained in the word sequence
+     * @return {@code true} if the word sequence contains the given word, else {@code false}
+     */
     public boolean contains(String word) {
         return words.contains(word.toLowerCase());
     }
 
+    /**
+     * Returns {@code true} if the word sequence contains the given words in their exact sequence, one after another
+     * without any other words in between, else {@code false}.
+     * <p>
+     * The check is performed case-insensitively.
+     * <p>
+     * A word sequence consisting of "get", "first" and "name" will return {@code true} when one of the following words
+     * are passed in the given order.
+     * <ul>
+     *     <li>"get"</li>
+     *     <li>"first"</li>
+     *     <li>"name"</li>
+     *     <li>"get" and "first"</li>
+     *     <li>"first" and "name</li>
+     *     <li>"get", "first" and "name</li>
+     * </ul>
+     * For any other word sequence, {@code false} would be returned.
+     *
+     * @param words the words possibly contained in the word sequence
+     * @return {@code true} if the word sequence contains the given words in their exact sequence, one after another
+     * without any other words in between, else {@code false}
+     */
     public boolean containsInSequence(String... words) {
         List<String> lowerCaseWords = toLowerCase(words);
         return indexOfSubList(this.words, lowerCaseWords) != -1;
     }
 
+    /**
+     * Returns {@code true} if the word sequence contains the given words in the given order. There may be additional
+     * words in between the given words.
+     * <p>
+     * The check is performed case-insensitively.
+     * <p>
+     * A word sequence consisting of "get", "first" and "name" will return {@code true} when one of the following words
+     * are passed in the given order.
+     * <ul>
+     *     <li>"get"</li>
+     *     <li>"first"</li>
+     *     <li>"name"</li>
+     *     <li>"get" and "first"</li>
+     *     <li>"get" and "name"</li>
+     *     <li>"first" and "name</li>
+     *     <li>"get", "first" and "name</li>
+     * </ul>
+     * For any other word sequence, {@code false} would be returned.
+     *
+     * @param words the words possibly contained in the word sequence
+     * @return {@code true} if the word sequence contains the given words in the given order
+     */
     public boolean containsInOrder(String... words) {
         List<String> lowerCaseWords = toLowerCase(words);
         Map<String, Integer> wordIndices = lowerCaseWords.stream().collect(toMap(identity(), this.words::indexOf));
@@ -65,6 +132,15 @@ public final class WordSequence {
         return true;
     }
 
+    /**
+     * Returns {@code true} if the word sequence contains the given words in any order.
+     * <p>
+     * A word sequence consisting of "get", "first" and "name" will return {@code true} for any combination and order of
+     * the three given words.
+     *
+     * @param words the words possibly contained in the word sequence
+     * @return {@code true} if the word sequence contains the given words in any order
+     */
     public boolean containsInAnyOrder(String... words) {
         return this.words.containsAll(toLowerCase(words));
     }
