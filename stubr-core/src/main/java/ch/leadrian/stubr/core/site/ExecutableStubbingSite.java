e2436337f7bf799d245c5828de3bdacd79433fa4
leadrian <thewishwithin@gmail.com>
2020-07-21 18:49:57 +0200

    Do not let ExecutableStubbingSite extend NamedStubbingSite

--- stubr-core/src/main/java/ch/leadrian/stubr/core/site/ExecutableStubbingSite.java
+++ stubr-core/src/main/java/ch/leadrian/stubr/core/site/ExecutableStubbingSite.java
@@ -16,13 +16,15 @@
 
 package ch.leadrian.stubr.core.site;
 
+import ch.leadrian.stubr.core.StubbingSite;
+
 import java.lang.reflect.Executable;
 
 /**
  * Stubbing site indicating that the current stubbing site is an {@link Executable}. Implementations of {@link
  * Executable} are {@link java.lang.reflect.Constructor} and {@link java.lang.reflect.Method}.
  */
-public interface ExecutableStubbingSite extends NamedStubbingSite {
+public interface ExecutableStubbingSite extends StubbingSite {
 
     /**
      * Returns the {@link Executable} where the stubbing takes place.
